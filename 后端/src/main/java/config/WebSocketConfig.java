85e0011dba42d2c70870056b619ff6bbc3e063ae
Zhoumiaolan <116415649+Zhoumiaolan@users.noreply.github.com>
2022-11-16 01:30:19 +0800

    Update WebSocketConfig.java

--- 后端/src/main/java/config/WebSocketConfig.java
+++ 后端/src/main/java/config/WebSocketConfig.java
@@ -1,13 +1,10 @@
-package com.lin.xiaoyaoshai.config;
+package config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-/**
- * 开启WebSocket支持
- * @author zhengkai.blog.csdn.net
- */
+
 @Configuration
 public class WebSocketConfig {
 
