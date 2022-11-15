54692169bf00b9fbad60c94825671c616762237e
Zhoumiaolan <116415649+Zhoumiaolan@users.noreply.github.com>
2022-11-16 01:25:04 +0800

    Update CodeMsg.java

--- 后端/src/main/java/config/CodeMsg.java
+++ 后端/src/main/java/config/CodeMsg.java
@@ -1,47 +1,35 @@
-package com.lin.xiaoyaoshai.config;
+package config;
 
 public class CodeMsg {
 
     private int code;
     private String msg;
 
-    //通用的错误码
+    
     public static CodeMsg NO_FOUND = new CodeMsg(400, "404");
     public static CodeMsg SUCCESS = new CodeMsg(0, "success");
-    public static CodeMsg SERVER_ERROR = new CodeMsg(500100, "服务端异常");
-    public static CodeMsg BIND_ERROR = new CodeMsg(500101, "参数校验异常：%s");
-    public static CodeMsg ACCESS_LIMIT_REACHED= new CodeMsg(500104, "访问高峰期，请稍等！");
-    //用户模块 5002XX
-    public static CodeMsg LOGIN_FAIL = new CodeMsg(500210, "登陆失败");
-    public static CodeMsg SEND_CODE_FAIL = new CodeMsg(500211, "发送失败");
-    public static CodeMsg REGISTER_FAIL = new CodeMsg(500212, "注册失败");
-    public static CodeMsg MATCH_WAIT = new CodeMsg(500215, "正在匹配");
-    public static CodeMsg DIS_ERROR = new CodeMsg(500216, "解除关系失败");
-
-    //订单模块 5003XX
-    public static CodeMsg ORDER_EMPTY = new CodeMsg(500310, "无订单");
-
-
-    //预约单模块 5004XX
-    public static CodeMsg IMG_NOT_RIGHT = new CodeMsg(500400, "上传图片格式不合法");
-    public static CodeMsg IMG_UPLOAD_FALSE = new CodeMsg(500401, "上传图片失败");
-
-    //秒杀模块 5005XX
-    public static CodeMsg SECKILL_OVER = new CodeMsg(500500, "商品已经秒杀完毕");
-    public static CodeMsg REPEATE_SECKILL = new CodeMsg(500501, "不能重复秒杀");
-
+    public static CodeMsg SERVER_ERROR = new CodeMsg(500100, "Server exception");
+    public static CodeMsg BIND_ERROR = new CodeMsg(500101, "Abnormal parameter verification：%s");
+    public static CodeMsg ACCESS_LIMIT_REACHED= new CodeMsg(500104, "Please wait for the peak time!");
+    public static CodeMsg LOGIN_FAIL = new CodeMsg(500210, "login failure");
+    public static CodeMsg SEND_CODE_FAIL = new CodeMsg(500211, "fail to send");
+    public static CodeMsg REGISTER_FAIL = new CodeMsg(500212, "fail to register");
+    public static CodeMsg MATCH_WAIT = new CodeMsg(500215, "Making a match");
+    public static CodeMsg DIS_ERROR = new CodeMsg(500216, "Failure to terminate the relationship");
+    public static CodeMsg ORDER_EMPTY = new CodeMsg(500310, "No orders");
+    public static CodeMsg IMG_NOT_RIGHT = new CodeMsg(500400, "The uploaded image format is invalid");
+    public static CodeMsg IMG_UPLOAD_FALSE = new CodeMsg(500401, "Uploading pictures Failed");
+    public static CodeMsg SECKILL_OVER = new CodeMsg(500500, "The goods have been sold in seconds");
+    public static CodeMsg REPEATE_SECKILL = new CodeMsg(500501, "You can't repeat the second kill");
     private CodeMsg() {
     }
-
     private CodeMsg(int code, String msg) {
         this.code = code;
         this.msg = msg;
     }
-
     public int getCode() {
         return code;
     }
-
     public void setCode(int code) {
         this.code = code;
     }
@@ -54,11 +42,7 @@ public class CodeMsg {
         this.msg = msg;
     }
 
-    /**
-     * 返回带参数的错误码
-     * @param args
-     * @return
-     */
+
     public CodeMsg fillArgs(Object... args) {
         int code = this.code;
         String message = String.format(this.msg, args);
